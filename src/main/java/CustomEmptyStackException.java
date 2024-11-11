94edf293d312ab2532748db505ec6eb90e77503a
Tutuxaaa <v.votchitseva@mail.ru>
2024-11-11 19:45:38 +0300

    other1

--- src/main/java/CustomEmptyStackException.java
+++ src/main/java/CustomEmptyStackException.java
@@ -1,4 +1,4 @@
-class CustomEmptyStackException extends Exception {
+public class CustomEmptyStackException extends Exception {
     public CustomEmptyStackException(String message) {
         super(message);
     }
